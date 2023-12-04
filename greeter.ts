2f13f3b8a7f8c2d0dc2efd234a5c54458b37028e
Jamie Baker <jamieson.baker88@gmail.com>
2023-12-04 11:49:02 +1000

    feat(#5): added the documentation (#6)
    * wip(#5): added readme
    
    * fix(#5): fixed typo in greeter

--- greeter.ts
+++ greeter.ts
@@ -1,9 +1,9 @@
 // console.log("Hello world.")
-function greetPerson(person:string): string {
-    return "Hello, " + person;
-  }
-   
-  var user = "Seetings Man";
-   
+function greetPerson(person: string): string {
+  return "Hello, " + person;
+}
+
+var user = "Settings Man";
+
 //   document.body.textContent = greeter(user);
 console.log(greetPerson(user));
