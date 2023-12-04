c6a928fbac0364df496a7fb9f5801cfd0d80af7e
Azariah Pundari <150247277+aztec-dev@users.noreply.github.com>
2023-12-04 11:08:24 +1000

    Create login screen (#1)
    * Refactor name.
    
    * Refactor name.

--- greeter.ts
+++ greeter.ts
@@ -3,7 +3,7 @@ function greetPerson(person:string): string {
     return "Hello, " + person;
   }
    
-  var user = "Jane User";
+  var user = "Jerome User";
    
 //   document.body.textContent = greeter(user);
 console.log(greetPerson(user));
